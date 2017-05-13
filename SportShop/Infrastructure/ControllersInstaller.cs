7fddbd670485839aa6f0370b586fddda6a9be063
Wojtek <wychu69@gmail.com>
2017-05-13 10:03:45 +0200

    Dodanie WindsorControllerFactory i ControllerInstaller
    Dodanie WindsorControllerFactory i ControllerInstaller

---
+++ SportShop/Infrastructure/ControllersInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace SportShop.Infrastructure
+{
+    // rejestrujemy klasy które chcemy zainstalować w kontenerze
+    public class ControllersInstaller: IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            //Rejestracja wszystkich klas które implementują interfejs IController
+            container.Register(Classes.FromThisAssembly().BasedOn<IController>().LifestyleTransient());
+        }
+    }
+}
---
+++ SportShop/Infrastructure/WindsorControllerFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.MicroKernel;
+
+namespace SportShop.Infrastructure
+{
+    public class WindsorControllerFactory : DefaultControllerFactory
+    {
+        //nadpisujemy domyślny sposób tworzenia kontrolerów
+        private readonly IKernel _kernel; //Kernel to worek w ktorym są zarejestrowane wszystkie klasy, metody itd.
+
+        public WindsorControllerFactory(IKernel kernel)
+        {
+            _kernel = kernel;
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            if (controllerType == null)
+            {
+                throw new HttpException(404, $"Controller for path {requestContext.HttpContext.Request.Path} could not be found");
+            }
+
+            
+            return (IController)_kernel.Resolve(controllerType); // metoda pobierająca z worka kontroler który zarejestrowaliśmy w kontenerze
+        }
+
+        public override void ReleaseController(IController controller)
+        {
+            _kernel.ReleaseComponent(controller); // czyścimy kontroler z worka
+        }
+    }
+}
