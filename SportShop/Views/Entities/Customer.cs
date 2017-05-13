988f9361ee514f4b9cc1e38b79c6e03271433fc9
Wojtek <wychu69@gmail.com>
2017-05-13 13:41:26 +0200

    Dodanie Entities product customer category

---
+++ SportShop/Views/Entities/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportShop.Views.Entities
+{
+    public enum Sex
+    {
+        Male, 
+        Female,
+        Unknown
+    }
+    public class Customer
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
---
+++ SportShop/Views/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportShop.Views.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
